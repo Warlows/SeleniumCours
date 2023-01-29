8eeb4f923a1c3074f82898fabf1bc2a5cbbb4c52
Paweł Drohomirecki <jin4ster@gmail.com>
2023-01-29 13:03:59 +0100

    task 3 done

---
+++ src/test/java/Konfiguracja/Zadanie3.java
@@ -0,0 +1,25 @@
+package Konfiguracja;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Zadanie3 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        driver.get("https://coderslab.pl/pl");
+        driver.navigate().back();
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        Thread.sleep(5000); // wtrzymanie na 5 sekund
+
+        driver.quit();
+    }
+}
